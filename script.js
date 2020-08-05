437ca542f33df8315aa963590434e5c764158ced
KarthikeyaAyyalaraju <67199163+KarthikeyaAP@users.noreply.github.com>
2020-08-05 15:16:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,27 +7,54 @@ const color=document.querySelector('#color');
 const search=document.querySelector('.search input');
 const ini=document.querySelector('#initial');
 
-const addTodo = todo => {
+let listItems=[];
+
+
+const addTodo = (todo,id) => {
     const html=`
-    <li class="list-group-item text-center text-light mx-20 px-30 justify-content-between align-items-center position-sticky align-middle">
-    <span>${todo}</span>
+    <li data-id="${id}" class="list-group-item text-center text-light mx-20 px-30 justify-content-between align-items-center position-sticky align-middle">
+    <span>${todo.title}</span>
     <i class="fa fa-trash delete"></i>
     </li>
     `;
     list.innerHTML += html;
 };
 
+const deleteTodo=(id) => {
+    const todos=document.querySelectorAll('li');
+    todos.forEach(todo => {
+        if(todo.getAttribute('data-id') == id){
+            todo.remove();
+        }
+    })
+}
+db.collection('todos').onSnapshot(snapshot =>{
+    snapshot.docChanges().forEach(change =>{
+        const doc=change.doc;
+        if(change.type == 'added'){
+            addTodo(doc.data(),doc.id);
+        }
+        else if(change.type=='removed'){
+            deleteTodo(doc.id);
+        }
+    })
+})
+
 form.addEventListener('submit',e=>{
     e.preventDefault();
     todo=text.value;
-    addTodo(todo);
-    text.value=null;
+    let obj={
+        'title':todo
+    }
+    db.collection('todos').add(obj)
+        form.reset();
    });
 
-   list.addEventListener('click',e=>{
-     if(e.target.classList.contains('delete')){
-         e.target.parentElement.remove();
-     };
+list.addEventListener('click',e=>{
+    if(e.target.classList.contains('delete')){
+        const id=e.target.parentElement.getAttribute('data-id');
+        db.collection('todos').doc(id).delete()
+    }
 });
 
 const filterTodos = (term) => {
@@ -44,3 +71,4 @@ search.addEventListener('keyup', () =>{
     const term=search.value;
     filterTodos(term);
 });
+console.log(Array.from(list.children))
